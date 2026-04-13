c645b3cdcd300c8da400ba1f6264e60e73864d8a
kd-nic <karsmaita@gmail.com>
2026-04-13 22:28:13 +0800

    Add files via upload

--- gdam_functions.py
+++ gdam_functions.py
@@ -3,7 +3,26 @@
 # Import any modules needed in your functions here
 import math
 
+
 # Define your new functions below
 def mean(x):
-    sum(mea
+    return sum(x) / len(x)
+
+
+def stddev(x):
+    #These define the N and mean to be used
+    n = len(x)
+    ave = mean(x)
+    #This makes the squared differences a list, so each item can be used by the function
+    squared_diff = [(i - ave) ** 2 for i in x]
+    
+    return math.sqrt(1/n * sum(squared_diff))
+
+def sem(x):
+    return stddev(x) / math.sqrt(len(x))
+
+
+def gaussian(gauss_mean, gauss_stddev, gauss_x):
+    #This calls on the predetermined variables gauss_mean and gauss_stddev, while gauss_x is a list whose individual contents must be called upon
+    return [(1/(gauss_stddev * math.sqrt(2*math.pi))) * math.e ** (-1*(((n - gauss_mean)**2) / ((2*gauss_stddev) **2))) for n in gauss_x]
 
